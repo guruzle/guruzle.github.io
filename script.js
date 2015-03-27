69ec041ad0e1eef2a37bdf3343aec9c6546689ba
Dan <jumbo@jumbo.com>
2015-03-27 17:08:12 -0400

    added new files

---
+++ script.js
@@ -0,0 +1,21 @@
+var guruzleApp = angular.module('guruzleApp',['ngRoute'])
+
+guruzleApp.config(function($routeProvider){
+	$routeProvider
+	.when('/',{
+		templateUrl : 'pages/home.html',
+		controller : 'mainController'
+	})
+	.when('/about',{
+		templateUrl : 'pages/about.html',
+		controller :  'aboutController'
+	});
+});
+//Controllers
+guruzleApp.controller('mainController', function($scope){
+	$scope.brand="Guruzle";
+	$scope.message="I am guruzle";
+});
+guruzleApp.controller('aboutController',function($scope){
+	$scope.message="About guruzle";
+})
