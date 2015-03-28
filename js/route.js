efe706d49d3cdac46eb2f5c70f26385265725503
Dan <jumbo@jumbo.com>
2015-03-28 11:57:24 -0400

    del route

--- js/route.js
+++
@@ -1,7 +0,0 @@
-// guruzleApp.config(function($routeProvider){
-// 	$routeProvider
-// 		.when( '/about', { controller: 'aboutCtrl', templateUrl: 'views/about.html' } )
-// 		.when('/', { controller: 'mainCtrl', templateUrl: 'views/home.html' })
-// 		.otherwise( { redirectTo: '/' } );
-// });			
-// 			
