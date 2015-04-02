47e2424def83a053712fa55df1d1ac9f8b08cf22
Dan <jumbo@jumbo.com>
2015-04-02 18:38:49 -0400

    adding a bunch of files

--- js/app.js
+++ js/app.js
@@ -5,5 +5,7 @@ guruzleApp.config(function($routeProvider, $locationProvider){
         when('/home', {templateUrl: 'pages/home.html', controller: 'homeCtrl'}).
         when('/team',{templateUrl: 'pages/team.html',controller: 'teamCtrl'}).
         when('/players',{templateUrl: 'pages/players.html',controller: 'playersCtrl'}).
+        when('/login',{templateUrl: 'pages/login.html',controller: 'loginCtrl'}).
+
         otherwise({ redirectTo: '/home' });
 });
--- js/controller.js
+++ js/controller.js
@@ -6,6 +6,36 @@ guruzleControllers.controller('homeCtrl', ['$scope', '$http',
       // $scope.phones = data;
     // });
 	$scope.brand = 'Guruzle';
-	$scope.message = 'Welcome to Guruzle';
+	$scope.message = 'Welcome to Guruzle - Home';
+	$scope.isActive = function (viewLocation) {return viewLocation === $location.path();}
+  }]);
+
+guruzleControllers.controller('teamCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+    // $http.get('phones/phones.json').success(function(data) {
+      // $scope.phones = data;
+    // });
+	$scope.brand = 'Guruzle - Team';
+	$scope.message = 'Welcome to Guruzle - Team';
+    // $scope.orderProp = 'age';
+  }]);
+
+guruzleControllers.controller('playersCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+    // $http.get('phones/phones.json').success(function(data) {
+      // $scope.phones = data;
+    // });
+	$scope.brand = 'Guruzle - Players';
+	$scope.message = 'Welcome to Guruzle - Players';
+    // $scope.orderProp = 'age';
+  }]);
+
+guruzleControllers.controller('loginCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+    // $http.get('phones/phones.json').success(function(data) {
+      // $scope.phones = data;
+    // });
+	$scope.brand = 'Guruzle - Login';
+	$scope.message = 'Welcome to Guruzle - Login';
     // $scope.orderProp = 'age';
   }]);
