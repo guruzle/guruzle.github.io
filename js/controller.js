1d4719890ac5161828005609713d075906350b82
root <attleboro_gunner_81@yahoo.com>
2015-07-10 15:23:30 -0700

    added new files

--- js/controller.js
+++ js/controller.js
@@ -2,40 +2,24 @@ var guruzleControllers = angular.module('guruzleControllers', []);
 
 guruzleControllers.controller('homeCtrl', ['$scope', '$http',
   function ($scope, $http) {
-    // $http.get('phones/phones.json').success(function(data) {
-      // $scope.phones = data;
-    // });
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+      alert("Geolocation is not supported by this browser.");
+  }
+  function showPosition(position) {
+    var loc = position.coords.latitude +   "," + position.coords.longitude; 
+    
+    $http.get('http://localhost:9000/twitter/' + loc).
+      success(function(data, status, headers, config) {
+        $scope.tweets = data;
+      }).
+      error(function(data, status, headers, config) {
+        $scope.tweets = data;
+      });
+  }
 	$scope.brand = 'Guruzle';
 	$scope.message = 'Welcome to Guruzle - Home';
 	$scope.isActive = function (viewLocation) {return viewLocation === $location.path();}
   }]);
 
-guruzleControllers.controller('teamCtrl', ['$scope', '$http',
-  function ($scope, $http) {
-    // $http.get('phones/phones.json').success(function(data) {
-      // $scope.phones = data;
-    // });
-	$scope.brand = 'Guruzle - Team';
-	$scope.message = 'Welcome to Guruzle - Team';
-    // $scope.orderProp = 'age';
-  }]);
-
-guruzleControllers.controller('playersCtrl', ['$scope', '$http',
-  function ($scope, $http) {
-    // $http.get('phones/phones.json').success(function(data) {
-      // $scope.phones = data;
-    // });
-	$scope.brand = 'Guruzle - Players';
-	$scope.message = 'Welcome to Guruzle - Players';
-    // $scope.orderProp = 'age';
-  }]);
-
-guruzleControllers.controller('loginCtrl', ['$scope', '$http',
-  function ($scope, $http) {
-    // $http.get('phones/phones.json').success(function(data) {
-      // $scope.phones = data;
-    // });
-	$scope.brand = 'Guruzle - Login';
-	$scope.message = 'Welcome to Guruzle - Login';
-  $scope.email = 'user@domain.com';
-  }]);
